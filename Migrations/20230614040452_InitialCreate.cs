a8cf55a09a0d9f25873a3d41e7757f1affedd81a
aeloh1m <melowgz@gmail.com>
2023-06-14 01:15:09 -0300

    Finished project, all the tests passed ok

---
+++ Migrations/20230614040452_InitialCreate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "movie",
+                columns: table => new
+                {
+                    idMovie = table.Column<int>(type: "int", nullable: false),
+                    movie_name = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    movie_genre = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    movie_duration = table.Column<int>(type: "int", nullable: false),
+                    movie_budget = table.Column<float>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.idMovie);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.CreateTable(
+                name: "actor",
+                columns: table => new
+                {
+                    idActor = table.Column<int>(type: "int", nullable: false),
+                    actor_name = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: false, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    actor_birthdate = table.Column<DateOnly>(type: "date", nullable: true),
+                    actor_picture = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true, collation: "utf8mb4_0900_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.idActor);
+                    table.ForeignKey(
+                        name: "Movie_Actor",
+                        column: x => x.idActor,
+                        principalTable: "movie",
+                        principalColumn: "idMovie");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "actor");
+
+            migrationBuilder.DropTable(
+                name: "movie");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EFCoreProject.Models;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EFCoreProject.Test")]
+
+namespace EFCoreProject
+{
+    public class Program
+    {
+        private static HollywoodContext _context;
+
+        static void Main(string[] args)
+        {
+            // Initialize the DbContext
+            _context = new HollywoodContext();
+
+            // Call the method to add a movie from input
+            AddMovieFromInput(_context);
+
+            // Other application logic...
+        }
+
+        public static void AddMovieFromInput(HollywoodContext context)
+        {
+            // Get user input for movie details
+            Console.WriteLine("Enter the movie name:");
+            string data1 = Console.ReadLine();
+            string movieName = data1;
+
+            Console.WriteLine("Enter the movie genre:");
+            string data2 = Console.ReadLine();
+            string movieGenre = data2;
+
+            Console.WriteLine("Enter the movie duration (in minutes):");
+            int data3 = int.Parse(Console.ReadLine());
+            int movieDuration = data3;
+
+            Console.WriteLine("Enter the movie budget:");
+            float data4 = float.Parse(Console.ReadLine());
+            float movieBudget = data4;
+
+            // Create a new movie object
+            var movie = new Movie
+            {
+                MovieName = movieName,
+                MovieGenre = movieGenre,
+                MovieDuration = movieDuration,
+                MovieBudget = movieBudget
+            };
+
+            // Add the movie to the database
+            context.Movies.Add(movie);
+            context.SaveChanges();
+
+            Console.WriteLine("Movie added successfully!");
+        }
+    }
+}
